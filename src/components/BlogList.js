924414666c73b67be254a6d4e30347eb2a72c4fa
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-10-11 12:32:18 +0300

    add image to bloglistcard

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -12,8 +12,9 @@ const BlogList = ({ blogs, title }) => {
         <h1 className="md:text-3xl text-2xl font-semibold my-2">{title}</h1>
         <div className="grid sm:grid-cols-3 md:gap-12 sm:gap-6">
           { blogs.map((blog) => (
-            <div key={blog.id} className="blogs-list bg-gray-200 md:p-6 p-4 rounded-md my-4">
-              <Link to={`/${blog.id}`} className="font-semibold">{blog.title}</Link>
+            <div key={blog.id} className="blogs-list bg-gray-200 md:p-6 p-4 rounded-lg my-4">
+              <img className="mb-4 w-full rounded-lg" src={blog.image_url} alt="Image url here.." />
+              <Link to={`/${blog.id}`} className="font-semibold text-pink-700 text-xl hover:text-gray-800">{blog.title}</Link>
               <h3>{blog.author}</h3>
               {/* <button className="bg-green-400 px-3 py-2 mt-2 rounded-md" onClick={() => handleDelete(blog.id)}>Delete Blog</button> */}
             </div>
