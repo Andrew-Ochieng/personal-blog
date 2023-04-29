3eceb945488f138052b4171e7b0def5218ebeb29
Code-X <andrewomosh44@gmail.com>
2023-04-29 15:28:52 +0300

    add blog-card styling

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -13,10 +13,10 @@ const BlogDetails = () => {
             <h1 className="md:text-3xl text-xl font-semibold md:mb-8 mb-4 text-gray-900">Blog Details - <span className="text-pink-500">{id}</span></h1>
             { isLoading && <h4 className="text-pink-500 font-semibold md:text-2xl text-xl">Loading data..</h4>}
             { error && <h4 className="text-red-500">{ error }</h4>}
-            {blog && (
+            {blog.data && (
                 <div className="flex items-center ">
                     <article className="">
-                        <h2 className="font-semibold md:text-3xl text-xl">{blog.title}</h2>
+                        <h2 className="font-semibold md:text-3xl text-xl">{blog.attributes.title}</h2>
                         <p className="text-xl text-pink-500 font-medium">Written by {blog.author}</p>
                         <img className="my-4 w-full" src={blog.image} alt="Details images here..." />
                         <div className="text-lg text-gray-800">{blog.content}</div>
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
+import useFetch from "./useFetch";
+import { FiChevronsRight } from "react-icons/fi"
 
 const BlogList = ({ blogs, title }) => {
-
-  console.log(blogs)
+  const comments = useFetch("http://localhost:1337/api/comments?populate=*")
+  
+  console.log(comments)
 
   
   return ( 
@@ -12,11 +15,13 @@ const BlogList = ({ blogs, title }) => {
           { blogs.map((blog) => (
             <div key={blog.id} className="blogs-list bg-gray-200 md:p-3 p-2 rounded-lg my-4">
               <img className="mb-4 w-full rounded-lg" src={`http://localhost:1337${blog.attributes.image.data.attributes.url}`} alt="" />
-              <Link to={`/${blog.id}`} className="hover:text-green-600" >
-                <h2 className="font-semibold text-pink-700 md:text-2xl text-xl hover:text-gray-800">{blog.title}</h2>
-                <h3 className="text-lg text-gray-800 font-medium">Written by: {blog.attributes.author}</h3>
-                <p>{blog.attributes.content.split(/\s+/).slice(0, 20).join(" ")}</p>
-              </Link>
+              <h2 className="font-semibold text-pink-700 md:text-2xl text-xl hover:text-gray-800">{blog.title}</h2>
+              <h3 className="text-lg text-gray-800 font-medium">Written by: {blog.attributes.author}</h3>
+              <p className="md:text-base text-sm font-light text-gray-800">{blog.attributes.content.split(/\s+/).slice(0, 20).join(" ")}</p>
+              <Link to={`/${blog.id}`} className="mt-2 font-medium md:text-base text-sm text-pink-500 hover:text-pink-700 flex items-center" >
+                  Read More
+                  <FiChevronsRight className="ml-1"/>              
+                </Link>
             </div>
           ))}
         </div>
